10cbe8e94f2a3def61e5f53b9e7c4fe177d72f8d
jgp <jgp@jgp.net>
2016-07-12 10:12:22 -0400

    Remote example

---
+++ src/main/java/net/jgp/labs/spark/ConnectRemotely.java
@@ -0,0 +1,23 @@
+/**
+ * Example on how to connect to a remote Spark server/cluster to see if it is live.
+ */
+package net.jgp.labs.spark;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
+
+/**
+ * @author jgp
+ *
+ */
+public class ConnectRemotely {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SparkConf conf = new SparkConf().setAppName("myApp").setMaster("spark://10.0.100.120:7077");
+		SparkContext sc = new SparkContext(conf);
+		System.out.println("Hello, Remote Spark v." + sc.version());
+	}
+}
