08cf0fc9cf2bbb7008d575b33d415795621fe62c
Jean Georges Perrin <jgp@jgp.net>
2016-10-10 12:10:23 -0400

    Work on JSON arrays.

--- src/main/java/net/jgp/labs/spark/JsonArrayToDataset.java
+++ src/main/java/net/jgp/labs/spark/JsonArrayToDataset.java
@@ -5,31 +5,25 @@ import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
 /**
- * A basic example of JSON conversion and simple manipulation on it.
+ * A basic example of JSON array.
  * @author jgp
  */
-public class JsonToDataset {
+public class JsonArrayToDataset {
 
 	public static void main(String[] args) {
 		System.out.println("Working directory = " + System.getProperty("user.dir"));
-		JsonToDataset app = new JsonToDataset();
+		JsonArrayToDataset app = new JsonArrayToDataset();
 		app.start();
 	}
 
 	private void start() {
 		SparkSession spark = SparkSession.builder().appName("JSON to Dataset").master("local").getOrCreate();
 
-		String filename = "data/north-carolina-school-performance-data.json";
+		String filename = "data/array.json";
 		long start = System.currentTimeMillis();
 		Dataset<Row> df = spark.read().json(filename);
 		long stop = System.currentTimeMillis();
 		System.out.println("Processing took " + (stop - start) + " ms");
-		df.show();
-		df.printSchema();
-
-		// Flatenization 
-		df = df.withColumn("district", df.col("fields.district"));
-		df = df.drop(df.col("fields.district")); // this does not work as the column stays here (Spark 2.0.0)
 		df.show();
 		df.printSchema();
 	}
