2f11f9aee5e613eef60789596f91f75b98e238b3
Jean Georges Perrin <jgp@jgp.net>
2017-08-17 14:02:38 -0500

    Got rid of this annying download dependency.

--- src/main/java/net/jgp/labs/spark/l900_analytics/ListNCSchoolDistricts.java
+++ src/main/java/net/jgp/labs/spark/l900_analytics/ListNCSchoolDistricts.java
@@ -7,7 +7,13 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
-import net.jgp.commons.io.download.DownloadManager;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.commons.io.FileUtils;
+
 
 /**
  * @author jgp
@@ -15,23 +21,34 @@ import net.jgp.commons.io.download.DownloadManager;
  */
 public class ListNCSchoolDistricts {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		String filename = DownloadManager.getFilename(
-				"https://opendurham.nc.gov/explore/dataset/north-carolina-school-performance-data/download/?format=json&timezone=America/New_York");
-		System.out.println("File " + filename + " downloaded");
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    String filename = "/tmp/" + System.currentTimeMillis() + ".json";
+    try {
+      FileUtils.copyURLToFile(
+          new URL("https://opendurham.nc.gov/explore/dataset/north-carolina-school-performance-data/download/?format=json&timezone=America/New_York"),
+          new File(filename));
+    } catch (MalformedURLException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    System.out.println("File " + filename + " downloaded");
 
-		SparkSession spark = SparkSession.builder().appName("NC Schools").master("local").getOrCreate();
+    SparkSession spark = SparkSession.builder().appName("NC Schools").master("local")
+        .getOrCreate();
 
-		String fileToAnalyze = "/tmp/" + filename;
-		System.out.println("File to analyze: " + fileToAnalyze);
+    String fileToAnalyze = "/tmp/" + filename;
+    System.out.println("File to analyze: " + fileToAnalyze);
 
-		Dataset<Row> df;
-		df = spark.read().option("dateFormat", "yyyy-mm-dd").json(fileToAnalyze);
-		df = df.withColumn("district", df.col("fields.district"));
-		df = df.groupBy("district").count().orderBy(df.col("district"));
-		df.show(150, false);
-	}
+    Dataset<Row> df;
+    df = spark.read().option("dateFormat", "yyyy-mm-dd").json(fileToAnalyze);
+    df = df.withColumn("district", df.col("fields.district"));
+    df = df.groupBy("district").count().orderBy(df.col("district"));
+    df.show(150, false);
+  }
 }
