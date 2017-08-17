1eb6fd74d51ca9f882947e4832426869f49fd2bc
jperrin <jperrin@WKS21986.essencehealthcare.COM>
2017-08-17 16:14:58 -0500

    More examples

---
+++ src/main/java/net/jgp/labs/spark/l000_ingestion/l500_dataset_book/Book.java
@@ -0,0 +1,5 @@
+package net.jgp.labs.spark.l000_ingestion.l500_dataset_book;
+
+public class Book {
+
+}
--- src/main/java/net/jgp/labs/spark/l250_map/l010/BookUrlBuilderApp.java
+++ src/main/java/net/jgp/labs/spark/l250_map/l010/BookUrlBuilderApp.java
@@ -8,34 +8,34 @@ import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
-public class BookMapperApp implements Serializable {
+public class BookUrlBuilderApp implements Serializable {
 	private static final long serialVersionUID = -5158472601494535920L;
 
-	private final class BookPrinter implements MapFunction<Row, String> {
-		private static final long serialVersionUID = 384463638437626547L;	
+	private final class BookUrlBuilder implements MapFunction<Row, String> {
+		private static final long serialVersionUID = 384463638437626547L;
 
 		@Override
 		public String call(Row r) throws Exception {
-			String s = r.getString(2) + " can be bought at " + r.getString(4);
+			String s = "<a href='" + r.getString(4) + "'>" + r.getString(2) + "</a>";
 			return s;
 		}
 	}
 
 	public static void main(String[] args) {
-		BookMapperApp app = new BookMapperApp();
+		BookUrlBuilderApp app = new BookUrlBuilderApp();
 		app.start();
 	}
 
 	private void start() {
-		SparkSession spark = SparkSession.builder().appName("For Each Book").master("local").getOrCreate();
+		SparkSession spark = SparkSession.builder().appName("Book URL Builder").master("local").getOrCreate();
 
 		String filename = "data/books.csv";
 		Dataset<Row> df = spark.read().format("csv").option("inferSchema", "true").option("header", "true")
 				.load(filename);
 		df.show();
 
-		Dataset<String> ds = df.map(new BookPrinter(), Encoders.STRING());
+		Dataset<String> ds = df.map(new BookUrlBuilder(), Encoders.STRING());
 		ds.printSchema();
-		ds.show();
+		ds.show(20, 80);
 	}
 }
