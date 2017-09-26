3975e5dc9a759dd809e3f3617d117b678c10bd94
Jean Georges Perrin <jgp@jgp.net>
2017-09-26 12:13:20 -0500

    S3...

--- src/main/java/net/jgp/labs/spark/l000_ingestion/l001_csv_in_progress/S3CsvToDataset.java
+++ src/main/java/net/jgp/labs/spark/l000_ingestion/l001_csv_in_progress/S3CsvToDataset.java
@@ -15,9 +15,16 @@ public class S3CsvToDataset {
         SparkSession spark = SparkSession.builder().appName("CSV on S3 to Dataset<Row>")
                 .master("local").getOrCreate();
 
-        String filename = "s3a://lumeris.data.platform.nonprod/PanamaCanal/dev/data/claim/EHIODS_0_1000_CLAIM_DTL";
-        Dataset<Row> df = spark.read().format("csv").option("inferSchema", "true")
+        //spark.sparkContext().hadoopConfiguration().set("fs.s3n.endpoint", "us-east-2");
+        String filename = "s3n://lumeris.data.platform.nonprod/PanamaCanal/dev/data/claim/EHIODS_0_1000_CLAIM_DTL";
+      
+        // dataset-001/EHIODS_0_1000_CLAIM
+        filename = "s3a://test-jgp-oregon/dataset-001/EHIODS_0_1000_CLAIM";//EHIODS_0_1000_CLAIM";
+        Dataset<Row> df = spark.read()
+                .format("csv")
+                .option("inferSchema", "true")
                 .option("header", "false")
+                .option("sep", "|")
                 .load(filename);
         df.show();
         df.printSchema();
