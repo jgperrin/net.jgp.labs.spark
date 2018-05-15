82a8ccce74811b78661e09d963faa70438f6ddca
Jean Georges Perrin <jgp@jgp.net>
2018-05-14 21:28:45 -0400

    Examples...

--- src/main/java/net/jgp/labs/spark/l000_ingestion/l700_java/ArrayToDataframeApp.java
+++ src/main/java/net/jgp/labs/spark/l000_ingestion/l700_java/ArrayToDataframeApp.java
@@ -7,25 +7,31 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.types.StringType;
 
+/**
+ * Look at https://github.com/jgperrin/net.jgp.books.sparkWithJava.ch03 for more
+ * explanations.
+ * 
+ * @author jgp
+ *
+ */
 public class ArrayToDataframeApp {
 
-    public static void main(String[] args) {
-        ArrayToDataframeApp app = new ArrayToDataframeApp();
-        app.start();
-    }
+  public static void main(String[] args) {
+    ArrayToDataframeApp app = new ArrayToDataframeApp();
+    app.start();
+  }
 
-    private void start() {
-        SparkSession spark = SparkSession.builder()
-                .appName("Array to Dataframe")
-                .master("local")
-                .getOrCreate();
+  private void start() {
+    SparkSession spark = SparkSession.builder()
+        .appName("Array to Dataframe (Dataset<Row>)")
+        .master("local")
+        .getOrCreate();
 
-        String[] l = new String[] { "a", "b", "c", "d" };
-        List<String> data = Arrays.asList(l);
-        Dataset<String> ds = spark.createDataset(data, Encoders.STRING());
-        Dataset<Row> df = ds.toDF();
-        df.show();
-    }
+    String[] l = new String[] { "a", "b", "c", "d" };
+    List<String> data = Arrays.asList(l);
+    Dataset<String> ds = spark.createDataset(data, Encoders.STRING());
+    Dataset<Row> df = ds.toDF();
+    df.show();
+  }
 }
--- src/main/java/net/jgp/labs/spark/l000_ingestion/l700_java/ArrayToDatasetApp.java
+++ src/main/java/net/jgp/labs/spark/l000_ingestion/l700_java/ArrayToDatasetApp.java
@@ -9,21 +9,20 @@ import org.apache.spark.sql.SparkSession;
 
 public class ArrayToDatasetApp {
 
-    public static void main(String[] args) {
-        ArrayToDatasetApp app = new ArrayToDatasetApp();
-        app.start();
-    }
+  public static void main(String[] args) {
+    ArrayToDatasetApp app = new ArrayToDatasetApp();
+    app.start();
+  }
 
-    private void start() {
-        SparkSession spark = SparkSession.builder()
-                .appName("Array to Dataset")
-//                .master("local")
-                .master("spark://10.0.100.81:7077")
-                .getOrCreate();
+  private void start() {
+    SparkSession spark = SparkSession.builder()
+        .appName("Array to Dataset<String>")
+        .master("local")
+        .getOrCreate();
 
-        String[] l = new String[] { "a", "b", "c", "d" };
-        List<String> data = Arrays.asList(l);
-        Dataset<String> df = spark.createDataset(data, Encoders.STRING());
-        df.show();
-    }
+    String[] l = new String[] { "a", "b", "c", "d" };
+    List<String> data = Arrays.asList(l);
+    Dataset<String> df = spark.createDataset(data, Encoders.STRING());
+    df.show();
+  }
 }
